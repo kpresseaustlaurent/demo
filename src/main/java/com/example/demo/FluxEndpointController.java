21d29986d05ef0d7267cc565f041878f7f62bd3b
Keven Presseau <EMAIL>
2023-06-08 13:14:59 -0400

    update with mono

--- src/main/java/com/example/demo/FluxEndpointController.java
+++ src/main/java/com/example/demo/FluxEndpointController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -24,8 +26,14 @@ public class FluxEndpointController {
                 .delayElements(Duration.ofMillis(DELAY))
                 .map(data -> "{\"value\": \"" + data + "\"}"));
     }
+    @GetMapping(value = "/mono-endpoint", produces = "application/json")
+    public ResponseEntity<Mono<String>> getMonoData() {
+        return ResponseEntity.ok().body(Mono.just(getMockStrings())
+                .delayElement(Duration.ofMillis(getMockStrings().size() * DELAY))
+                .map(data -> "{\"value\": \"" + data + "\"}"));
+    }
     @GetMapping(value = "/paging-endpoint", produces = "application/json")
-    public ResponseEntity<Page<String>> getStrings (
+    public ResponseEntity<Page<String>> getPagedData (
             @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
             @RequestParam(value = "page", defaultValue = "0") int page) throws InterruptedException {
 
